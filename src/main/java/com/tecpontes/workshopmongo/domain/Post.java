650490ed9bda4772e9c82924cee69e09eb032403
Leonardo Pontes <leopontes.1989@gmail.com>
2021-06-17 11:46:02 -0300

    Acrescentando comentarios ao post

--- src/main/java/com/tecpontes/workshopmongo/domain/Post.java
+++ src/main/java/com/tecpontes/workshopmongo/domain/Post.java
@@ -1,12 +1,15 @@
 package com.tecpontes.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.tecpontes.workshopmongo.dto.AuthorDTO;
+import com.tecpontes.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -18,6 +21,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	private Post() {
 		
@@ -72,6 +77,14 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -97,6 +110,4 @@ public class Post implements Serializable {
 		return true;
 	}
 
-	
-	
 }
